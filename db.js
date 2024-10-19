7855a10a8664da3243ac5b0042b370f2795ab45c
aaron weiss <aaronweiss00@gmail.com>
2024-10-19 19:14:48 +0300

    newchanges

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-// Create a new Sequelize instance and connect to your database
-const sequelize = new Sequelize('paintings_db', 'aaron', 'abc', {
-    host: 'localhost',
+
+// Use environment variables for sensitive information
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
 });
 
-// Test the connection to make sure Sequelize is working correctly
 sequelize.authenticate()
     .then(() => {
         console.log('Connection has been established successfully.');
