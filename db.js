964a9f89d3fbcad4615956f7dc85929808a4caf8
the-true-kid <aaronweiss00@gmail.com>
2024-12-03 17:42:32 +0200

    added production and deployment syntax in db

--- db.js
+++ db.js
@@ -8,6 +8,9 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT || 5432, // Default to 5432 if not specified
+    ssl: {
+        rejectUnauthorized: false, // Required for Render's external connections
+    },
 });
 
 // A helper function for querying the database
